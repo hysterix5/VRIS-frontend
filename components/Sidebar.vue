0f7b416a7d2a38278e1ac6a97a9cef13e41ffa00
hysterix5 <denopolred@gmail.com>
2024-04-17 16:36:03 +0800

    test

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -38,6 +38,10 @@
         <div class="grid place-items-center mr-4"><Icon name="material-symbols:logout" style="width: 24px; height: 24px;" /></div>
         <button @click="logout">Log Out</button>
       </div>
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:logout" style="width: 24px; height: 24px;" /></div>
+        <button>Sample</button>
+      </div>
     </nav>
   </div>
 </template>
