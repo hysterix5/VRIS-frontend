56945c5ab7a8e3e39e40ad5e51224440156696b2
hysterix5 <denopolred@gmail.com>
2024-04-17 16:16:27 +0800

    added icons

--- components/Dataform.vue
+++ components/Dataform.vue
@@ -26,7 +26,7 @@
         <div v-if="selectedApprehensionType === 'individual' || selectedApprehension === 'sobriety'">
           <!-- Individual Form -->
           <section>
-            <form>
+            <form @submit.prevent="submitViolator">
     <h1 class="font-bold p-4">Violator</h1>
     <hr class="border-t-2 border-black">
     <div class="grid grid-rows-3">
@@ -36,20 +36,20 @@
         <div class="border-b border-black">
             <div class="m-3">
                 <label for="name" class="block">FIRST NAME<span class="text-red-600">*</span></label>
-                <input v-model="violator.firstname" type="text" name="name" id="name" class="w-full border">
+                <input v-model="violator.firstname" type="text" class="w-full border">
             </div>
          
         </div>
         <div class="border border-black border-t-0 ">
             <div class="m-3">
                 <label for="course" class="block">MIDDLE NAME</label>
-                <input v-model="violator.middlename" type="text" name="course" id="course" class="w-full border">
+                <input v-model="violator.middlename" type="text" class="w-full border">
             </div>
         </div>
         <div class="border-b border-black">
             <div class="m-3">
                 <label for="year" class="block">LAST NAME<span class="text-red-600">*</span></label>
-                <input v-model="violator.lastname" type="text" name="year" id="year" class="w-full border">
+                <input v-model="violator.lastname" type="text" class="w-full border">
             </div>
         </div>
       </div>
@@ -61,11 +61,11 @@
              <div class="grid grid-cols-2">
                 <div class="pr-3">
                 <label for="name" class="block">SUFFIX<span class="text-red-600">*</span></label>
-                <input v-model="violator.suffix" type="text" name="name" id="name" class="w-full border">
+                <input v-model="violator.suffix" type="text" class="w-full border">
             </div>
              <div>
                 <label for="name" class="block">SEX<span class="text-red-600">*</span></label>
-                <input v-model="violator.sex" type="text" name="name" id="name" class="w-full border">
+                <input v-model="violator.sex" type="text" class="w-full border">
              </div>
             </div>
          </div>
@@ -75,7 +75,7 @@
         <div class="border border-black border-t-0">
             <div class="m-3">
                 <label class="block">BIRTHDATE</label>
-                <input v-model="violator.bday" type="date" class="w-full border">
+                <input v-model="violator.birthdate" type="date" class="w-full border">
             </div>
         </div>
 
@@ -105,6 +105,9 @@
         </div>
       </div>
     </div>
+    <div class="flex justify-end p-4">
+              <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            </div>
     </form>
         </section>
         </div>
@@ -113,7 +116,7 @@
           <!-- Establishment Form -->
           <section>
 
-           <form>
+           <form @submit.prevent="submitEstablishment">
             <h1 class="font-bold p-4">Establishment</h1>
             <hr class="border-t-2 border-black">
             <div class="grid grid-rows-4">
@@ -129,7 +132,7 @@
                     <div class="border border-black border-t-0 border-r-0">
                         <div class="m-3">
                             <label for="course" class="block">TYPE OF ESTABLISHMENT</label>
-                            <input v-model="establishment.type" type="text" name="course" id="course" class="w-full border">
+                            <input v-model="establishment.establishment_type" type="text" name="course" id="course" class="w-full border">
                         </div>
                     </div>
                 </div>
@@ -139,7 +142,7 @@
                     <div class="border border-black border-t-0 border-l-0">
                         <div class="m-3">
                             <label class="block">REGISTERED OWNER</label>
-                            <input v-model="establishment.registeredOwner" type="text" class="w-full border">
+                            <input v-model="establishment.registered_owner" type="text" class="w-full border">
                         </div>
                     </div>
                     <div class="border-b border-black">
@@ -167,6 +170,9 @@
                 </div>
 
             </div>
+            <div class="flex justify-end p-4">
+              <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            </div>
         </form>
         </section>
         </div>
@@ -174,7 +180,7 @@
         <div v-else-if="selectedApprehensionType === 'publicConveyances'">
           <!-- Public Conveyances Form -->
           <section>
-            <form>
+            <form @submit.prevent="submitPublicConveyances">
             <h1 class="font-bold p-4">Public Conveyances</h1>
             <hr class="border-t-2 border-black">
             <div class="grid grid-rows-3">
@@ -184,13 +190,13 @@
                     <div class="border-b border-black">
                         <div class="m-3">
                             <label for="name" class="block">NAME OF DRIVER<span class="text-red-600">*</span></label>
-                            <input v-model="public_conveyances.drivername" type="text" name="name" id="name" class="w-full border">
+                            <input v-model="public_conveyances.driver_name" type="text" name="name" id="name" class="w-full border">
                         </div>
                     </div>
                     <div class="border border-black border-t-0 border-r-0 col-span-2">
                         <div class="m-3">
                             <label for="course" class="block">PLACE OF APPREHENSION</label>
-                            <input v-model="public_conveyances.apprehensionPlace" type="text" name="course" id="course" class="w-full border">
+                            <input v-model="public_conveyances.apprehension_place" type="text" name="course" id="course" class="w-full border">
                         </div>
                     </div>
                 </div>
@@ -200,13 +206,13 @@
                     <div class="border border-black border-t-0 border-l-0">
                         <div class="m-3">
                             <label class="block">REGISTERED OWNER</label>
-                            <input v-model="public_conveyances.registeredOwner" type="text" class="w-full border">
+                            <input v-model="public_conveyances.registered_owner" type="text" class="w-full border">
                         </div>
                     </div>
                     <div class="border-b border-black">
                         <div class="m-3">
                             <label class="block">OWNER'S ADDRESS<span class="text-red-600">*</span></label>
-                            <input v-model="public_conveyances.ownerAddress" type="text" class="w-full border">
+                            <input v-model="public_conveyances.owner_address" type="text" class="w-full border">
                         </div>
                     </div>
                 </div>
@@ -217,18 +223,21 @@
                     <div class="border border-black border-t-0 border-l-0">
                         <div class="m-3">
                             <label class="block">PLATE NO.</label>
-                            <input v-model="public_conveyances.plateNumber" type="text" class="w-full border">
+                            <input v-model="public_conveyances.plate_no" type="text" class="w-full border">
                         </div>
                     </div>
 
                     <div class="border border-black border-t-0 border-x-0">
                         <div class="m-3">
                             <label class="block">LICENSE NO.</label>
-                            <input v-model="public_conveyances.licenseNumber" type="text" class="w-full border">
+                            <input v-model="public_conveyances.license_no" type="text" class="w-full border">
                         </div>
                     </div>
                 </div>
 
+            </div>
+            <div class="flex justify-end p-4">
+              <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
             </div>
         </form>
         </section>
@@ -237,67 +246,139 @@
     </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        selectedApprehension: '', // Selected apprehension
-        selectedApprehensionType: '', // Selected liquor type
-        selectedAntiSmokingType: '', // Selected anti-smoking type
-        cigaretteClassification: '', // Input field value for classification of cigarettes
-        apprehensions: [
-          { label: 'Sobriety', value: 'sobriety' },
-          { label: 'Liquor', value: 'liquor' },
-          { label: 'Anti-Smoking', value: 'antismoking' }
-        ],
-        apprehensionTypes: [
-          { label: 'Individual', value: 'individual' },
-          { label: 'Establishment', value: 'establishment' },
-          { label: 'Public Conveyances', value: 'publicConveyances' }
-        ],
-      };
-    },
-
-    setup () {
-        violator =
-            {
-            firstname: '',
-            middlename: '',
-            lastname: '',
-            sex: '',
-            address: '',
-            bday: '',
-            suffix: '',
-            occupation: '',
-            dqrcode: '',
-            };
-      
-        establishment = 
-            {
-            name: '',
-            address: '',
-            registeredOwner: '',
-            permit: '',
-            type: '',
-            remarks: '',
-            },
-      
-        public_conveyances =
-            {
-            drivername: '',
-            apprehensionPlace: '',
-            licenseNumber: '',
-            plateNumber: '',
-            registeredOwner: '',
-            ownerAddress: '',
-            };
-
-        return {
-            violator,
-            establishment,
-            public_conveyances,
-            }
+ <script>
+ import axios from 'axios';
+ 
+ export default {
+   data() {
+     return {
+       selectedApprehension: '', // Selected apprehension
+       selectedApprehensionType: '', // Selected liquor type
+       selectedAntiSmokingType: '', // Selected anti-smoking type
+       cigaretteClassification: '', // Input field value for classification of cigarettes
+       apprehensions: [
+         { label: 'Sobriety', value: 'sobriety' },
+         { label: 'Liquor', value: 'liquor' },
+         { label: 'Anti-Smoking', value: 'antismoking' }
+       ],
+       apprehensionTypes: [
+         { label: 'Individual', value: 'individual' },
+         { label: 'Establishment', value: 'establishment' },
+         { label: 'Public Conveyances', value: 'publicConveyances' }
+       ],
+       violator: {
+         firstname: '',
+         middlename: '',
+         lastname: '',
+         sex: '',
+         address: '',
+         birthdate: '',
+         suffix: '',
+         occupation: '',
+         dqrcode: '',
+         apprehension_type: '',
+       },
+       establishment: {
+         name: '',
+         address: '',
+         registered_owner: '',
+         permit: '',
+         establishment_type: '',
+         remarks: '',
+         apprehension_type: '',
+       },
+       public_conveyances: {
+         driver_name: '',
+         apprehension_place: '',
+         license_no: '',
+         plate_no: '',
+         registered_owner: '',
+         owner_address: '',
+         apprehension_type: '',
+       }
+     };
+   },
+   methods: {
+    async submitViolator() {
+    try {
+      this.violator.apprehension_type = this.selectedApprehension; 
+      console.log(this.violator)
+      const response = await axios.post('http://localhost:8000/api/violator', this.violator);
+      console.log('Response:', response.data);
+      // Reset form fields after successful submission
+      this.resetViolatorForm();
+      alert('Violator data submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting violator data:', error);
+      alert('Error submitting violator data. Please try again.');
     }
-  };
-  </script>
-  
+  },
+  async submitEstablishment() {
+    try { 
+      this.establishment.apprehension_type = this.selectedApprehension; // Assign selected apprehension to the form data
+      const response = await axios.post('http://localhost:8000/api/establishment', this.establishment);
+      console.log('Response:', response.data);
+      // Handle submission of establishment form
+      console.log('Establishment form submitted:', this.establishment);
+      // Reset form fields after submission
+      this.resetEstablishmentForm();
+      alert('Establishment data submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting establishment data:', error);
+      alert('Error submitting establishment data. Please try again.');
+    }
+  },
+  async submitPublicConveyances() {
+    try {
+      this.public_conveyances.apprehension_type = this.selectedApprehension; // Assign selected apprehension to the form data
+      const response = await axios.post('http://localhost:8000/api/public', this.public_conveyances);
+      console.log('Response:', response.data);
+      // Handle submission of public conveyances form
+      console.log('Public Conveyances form submitted:', this.public_conveyances);
+      // Reset form fields after submission
+      this.resetPublicConveyancesForm();
+      alert('Public Conveyances data submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting public conveyances data:', error);
+      alert('Error submitting public conveyances data. Please try again.');
+    }
+  },
+     resetViolatorForm() {
+       // Reset violator form fields
+       this.violator = {
+         firstname: '',
+         middlename: '',
+         lastname: '',
+         sex: '',
+         address: '',
+         birthdate: '',
+         suffix: '',
+         occupation: '',
+         dqrcode: ''
+       };
+     },
+     resetEstablishmentForm() {
+       // Reset establishment form fields
+       this.establishment = {
+         name: '',
+         address: '',
+         registeredOwner: '',
+         permit: '',
+         type: '',
+         remarks: ''
+       };
+     },
+     resetPublicConveyancesForm() {
+       // Reset public conveyances form fields
+       this.public_conveyances = {
+         drivername: '',
+         apprehensionPlace: '',
+         licenseNumber: '',
+         plateNumber: '',
+         registeredOwner: '',
+         ownerAddress: ''
+       };
+     }
+   }
+ };
+ </script>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,36 +1,47 @@
 <template>
-  <!-- component -->
+  <!-- Sidebar component -->
   <div class="flex flex-col bg-white text-gray-700 h-screen w-full max-w-[20rem]">
+    <!-- Logo and title -->
     <div class="mb-6 mt-3 p-4 flex flex-col items-center text-center justify-center">
       <NuxtImg src="/img/vris_logo.png" sizes="75px"/>
       <h5 class="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">Vices Regulation Information Systems</h5>
     </div>
+    <!-- Sidebar navigation -->
     <nav class="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+      <!-- User Account -->
       <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4"></div>
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:account-circle-full" style="width: 24px; height: 24px;" /></div>
         <NuxtLink to="/main">User Account</NuxtLink>
       </div>
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4">
-          <NuxtLink to="/main">System User</NuxtLink>
-        </div>
+      <!-- System User -->
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:person" style="width: 24px; height: 24px;" /></div>
+        <NuxtLink to="/main">System User</NuxtLink>
       </div>
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4"></div>
+      <!-- Data Entry -->
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:list-alt-add" style="width: 24px; height: 24px;" /></div>
         <NuxtLink to="/main/data_entry">Data Entry</NuxtLink>
       </div>
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4">Profile</div>
+      <!-- Reports -->
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:bar-chart" style="width: 24px; height: 24px;" /></div>
+        <NuxtLink to="/main">Reports</NuxtLink>
       </div>
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <div class="grid place-items-center mr-4">Settings</div>
+      <!-- Settings -->
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:settings" style="width: 24px; height: 24px;" /></div>
+        <NuxtLink to="/main">Settings</NuxtLink>
       </div>
-      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
-        <button @click="logout" class="grid place-items-center mr-4">Log Out</button>
+      <!-- Log Out -->
+      <div role="button" tabindex="0" class="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none">
+        <div class="grid place-items-center mr-4"><Icon name="material-symbols:logout" style="width: 24px; height: 24px;" /></div>
+        <button @click="logout">Log Out</button>
       </div>
     </nav>
   </div>
 </template>
+
 
 <script>
 
